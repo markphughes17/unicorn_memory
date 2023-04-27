208ef5b88fa34d49f5d418bbce014f0566998e6b
Mark Hughes <markphughes17@hotmail.co.uk>
2023-04-27 18:52:12 +0100

    Create Menu Scene and added empty settings and easyGameScene

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+
+    public void Settings() {
+        //TODO Settings scene
+        //SceneManager.LoadScene()
+    }
+
+    public void EasyGame() {
+        //TODO Easy scene
+    }
+
+    public void MedGame() {
+        //TODO Medium scene
+    }
+
+    public void HardGame() {
+        //TODO Hard scene
+    }
+}
